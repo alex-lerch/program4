aac3f935ce32daf63bd82a1eaa64654518c27e35
alex-lerch <106268256+alex-lerch@users.noreply.github.com>
2023-10-19 13:36:28 -0500

    disjoint set works for the provided tester file and as such is completed

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -1,23 +1,60 @@
 // DisjointSet implementation using union by size and path compression
-// By Mary Elaine Califf and
+// By Mary Elaine Califf and Alex Lerch
 
 #include "DisjointSet.h"
 #include <iostream>
 
-DisjointSet::DisjointSet(int numObjects)
+DisjointSet::DisjointSet(int numObjects) : theArray(numObjects, -1)
 {
-    //to do
+    numValues = numObjects;
 }
 
 //recursive method to find the item -- does path compression on the way out of the recursion
 int DisjointSet::find(int objectIndex)
 {
-    // to do -- see assignment instructions for details
+    // if index is a root then return that index
+    if (theArray[objectIndex] < 0) {
+        return objectIndex;
+    }
+    // the index is not a root and so we must look at index's parent to see if it is the root
+    else {
+        int setName = find(theArray[objectIndex]);
+        theArray[objectIndex] = setName;
+        return setName;
+    }
 }
 
 bool DisjointSet::doUnion(int objIndex1, int objIndex2)
 {
-    // to do -- see assignment instructions for details
+    // get the roots of objIndex1 and objIndex2
+    int root1 = find(objIndex1);
+    int root2 = find(objIndex2);
+
+    // if root1 and root2 are in different sets, then proceed with unioning them
+    if (root1 != root2) {
+        // if root1 has fewer items in the set than root2, then assign root1 to point at root2
+        if (theArray[root1] > theArray[root2]) {
+            // update the size
+            theArray[root2] += theArray[root1];
+            // assign root1 to point at root2
+            theArray[root1] = root2;
+        }
+        // root1 has either fewer or the same number of items as root2. assign root2 to point at root1
+        else {
+            // update the size
+            theArray[root1] += theArray[root2];
+            // assign root2 to point at root1
+            theArray[root2] = root1;
+        }
+        
+        // check for complete set union
+        if ( (theArray[root1] == (numValues * -1) ) || (theArray[root2] == (numValues * -1) ) ) {
+            return true;
+        }
+    }
+
+    // the set has not been completely unioned so return false
+    return false;
 }
 
 void DisjointSet::printArrayValues(std::ostream &outputStream)
